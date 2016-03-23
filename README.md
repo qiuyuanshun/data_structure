dd599857d53b306e40b8a437ee5eaad9d477bf9b
qiuyuanshun <qiuyuanshun@yeah.net>
2016-03-23 23:22:55 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # data_structure
+这是c的数据结构的一些练习。
---
+++ ch1.c
@@ -0,0 +1,13 @@
+ ///---------------------------------------///
+ /// @file is the ch1.c
+ /// @arthor is the Arthur(qiuyuanshun@yeah.net)
+ /// @date:2016-03-23 21:47:37
+ ///------------------------------------------///
+#include <stdio.h>
+
+void bubble.sort(*int a[],int n )
+{
+     for(i = n- 1,change = true;i>=1&&chang;--i)
+         change = false;
+     for(j=0;j<i;++j)
+         if
